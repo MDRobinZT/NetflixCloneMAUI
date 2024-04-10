451fd5a29e4cf36dbbda2def30e1d024b170f406
Opilane TTHK <opilane@tthk.ee>
2024-04-10 11:27:07 +0300

    30:00

---
+++ NetflixCloneMAUI/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace NetflixCloneMAUI.Pages;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage(TmdbService tmdbService)
+	{
+		InitializeComponent();
+	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await _homeViewModel.InitializeAsync();
+    }
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
